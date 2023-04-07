9ceb80fa3efcd871dabb4d392166e20d24002d74
abhilashdk2016 <65328853+abhilashdkadv@users.noreply.github.com>
2023-04-07 16:25:12 +0530

    Login Screen

--- pages/cart.js
+++ pages/cart.js
@@ -84,7 +84,7 @@ function CartPage() {
                                 </div>
                             </li>
                             <li>
-                                <button onClick={() => router.push('/shipping')} className='primary-button w-full'>Checkout</button>
+                                <button onClick={() => router.push('login?redirect=/shipping')} className='primary-button w-full'>Checkout</button>
                             </li>
                         </ul>
                     </div>
