5a990cb89330877c8d1bcf3d67ac6f96f1586d78
abhilashdk2016 <65328853+abhilashdkadv@users.noreply.github.com>
2023-04-07 09:19:56 +0530

    React Context - Add to Cart

--- components/Layout.js
+++ components/Layout.js
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Head from 'next/head'
 import Link from 'next/link'
+import { Store } from '@/utils/Store'
 
 export default function Layout({ children, title }) {
+    const { state, dispatch } = useContext(Store);
+    const { cart } = state;
   return (
     <>
         <Head>
@@ -18,7 +21,12 @@ export default function Layout({ children, title }) {
                     <a className='text-lg font-bold'>Amazon</a>
                 </Link>
                 <div>
-                    <Link href="/cart" legacyBehavior><a className="p-2">Cart</a></Link>
+                    <Link href="/cart" legacyBehavior><a className="p-2">Cart {
+                        cart.cartItems.length > 0 
+                        && <span className='ml-1 rounded-full bg-red-600 px-2 py-1 text-xs font-bold text-white'>
+                            {cart.cartItems.reduce((a, c) => a + c.quantity, 0 )}
+                        </span> 
+                    }</a></Link>
                     <Link href="/login" legacyBehavior><a className="p-2">Login</a></Link>
                 </div>
             </nav>
