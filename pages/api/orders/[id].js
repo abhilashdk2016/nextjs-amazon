6eb4ce51d0e3cd76111e53abc635aa3f6cee9712
abhilashdk2016 <65328853+abhilashdkadv@users.noreply.github.com>
2023-04-09 12:14:24 +0530

    Order Screen

--- pages/api/orders/[id].js
+++ pages/api/orders/[id].js
@@ -3,13 +3,9 @@ import db from "@/utils/db";
 
 const handler = async (req, res) => {
     await db.connect();
-    const newOrder = new Order({
-        ...req.body.order,
-        user: req.body.user._id
-    });
-    const order = await newOrder.save();
+    const order = await Order.findById(req.query.id);
     await db.disconnect();
-    res.status(201).send(order);
+    res.status(200).send(order);
 }
 
 export default handler;
